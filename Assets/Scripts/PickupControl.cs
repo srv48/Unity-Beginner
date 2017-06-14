0f2029122f39669eeedb22b0e4814077eba4d7e8
Sourav Bhattacharjee <srvb1729@gmail.com>
2017-06-14 17:17:10 +0530

    Add files via upload
    First Commit

---
+++ Assets/Scripts/PickupControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupControl : MonoBehaviour {
+
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (30, 30, 45) * Time.deltaTime);
+		
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+	private Rigidbody rb;
+	public float speed;
+	private int count;
+	public Text countText, winText;
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		countText.text = "Count : " + count.ToString ();
+		winText.text = "";
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal, moveVertical;
+		moveHorizontal = Input.GetAxis ("Horizontal");
+		moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Collect")) {
+			other.gameObject.SetActive (false);
+			count++;
+			countText.text = "Count : " + count.ToString ();
+		}
+		if(count == 10)
+			winText.text = "You win!";
+	}
+}
